ad2195cc6e1eef2bfbb924abee17ceeb5d644053
julien-lav <julien.laville@hotmail.fr>
2019-06-24 14:47:53 +0200

    hello les louploups

---
+++ src/Service/Calculator.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+class Calculator
+{
+	public function add(int $firstNumber, int $secondeNumber): int
+	{
+		return $firstNumber + $secondeNumber;
+	}
+}
---
+++ tests/Service/ServiceTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\Service\Calculator;
+use PHPUnit\Framework\TestCase;
+
+class CalculatorTest extends TestCase
+{
+    public function testAdd()
+    {
+        $calculator = new Calculator();
+        $result = $calculator->add(1, 2);
+
+        $this->assertEquals(3, $result);
+    }
+}
+Réduire
+
+
+
+14 h 38
