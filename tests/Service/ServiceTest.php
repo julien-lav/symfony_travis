69b8947836d1a1daeaee58de7d26e45d7187ed8a
julien-lav <julien.laville@hotmail.fr>
2019-06-24 16:46:35 +0200

    slayer

--- tests/Service/ServiceTest.php
+++ tests/Service/ServiceTest.php
@@ -10,8 +10,8 @@ class CalculatorTest extends TestCase
     public function testAdd()
     {
         $calculator = new Calculator();
-        $result = $calculator->add(1, 2);
+        $result = $calculator->add(2, 2);
 
-        $this->assertEquals(3, $result);
+        $this->assertEquals(4, $result);
     }
 }
